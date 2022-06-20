b02862fb0915fb8df259283219d8ffba23b9089c
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-06-20 16:37:16 +0300

    dz22
    dz22

--- Program.cs
+++ Program.cs
@@ -7,41 +7,41 @@ namespace vn_mode_csharp_dz22
         static void Main(string[] args)
         {
             int arraySize = 30;
-            int[] numbers = new int[arraySize];
+            int[] arrayNumbers = new int[arraySize];
             int minIntNumber = 1;
             int maxIntNumber = 10;
             Random random = new Random();
 
             for (int i = 0; i < arraySize; i++)
             {
-                numbers[i] = random.Next(minIntNumber, maxIntNumber);
+                arrayNumbers[i] = random.Next(minIntNumber, maxIntNumber);
             }
 
             Console.WriteLine("Исходный массив целых чисел:");
 
-            foreach (var item in numbers)
+            foreach (var item in arrayNumbers)
             {
                 Console.Write(item + " ");
             }
 
             Console.WriteLine("\n\nВсе локальные максимумы: ");
 
-            if (numbers[0] > numbers[1])
+            if (arrayNumbers[0] > arrayNumbers[1])
             {
-                Console.Write(numbers[0] + " ");
+                Console.Write(arrayNumbers[0] + " ");
             }
 
             for (int i = 0; i < arraySize - 2; i++)
             {
-                if(numbers[i + 1] > numbers[i] && numbers[i + 1] > numbers[i + 2])
+                if(arrayNumbers[i + 1] > arrayNumbers[i] && arrayNumbers[i + 1] > arrayNumbers[i + 2])
                 {
-                    Console.Write(numbers[i + 1] + " ");
+                    Console.Write(arrayNumbers[i + 1] + " ");
                 }
             }
 
-            if (numbers[arraySize - 1] > numbers[arraySize - 2])
+            if (arrayNumbers[arraySize - 1] > arrayNumbers[arraySize - 2])
             {
-                Console.Write(numbers[arraySize - 1] + " ");
+                Console.Write(arrayNumbers[arraySize - 1] + " ");
             }
 
         }
